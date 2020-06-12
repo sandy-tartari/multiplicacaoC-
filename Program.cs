25ec67a3ec2412dbecc1121a54b89845b83d1b11
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-12 20:39:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace multiplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algorirmo para multiplicar dois números
+            int n1, n2, multiplicacao;
+            Console.WriteLine("Escreva um número (e tecle ENTER)");
+            n1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Escreva umm número (e tecle ENTER)");
+            n2 = int.Parse(Console.ReadLine());
+            
+            multiplicacao = n1 * n2; 
+            
+            Console.WriteLine("O resultado é {0}.", multiplicacao);
+        }
+    }
+}
